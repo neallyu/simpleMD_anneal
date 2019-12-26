549df37f746d06106c938895336163f84a0ca3fd
Neal <neallyu9210@outlook.com>
2019-12-26 13:27:44 +0800

    bug fix

--- script/make_plot_trend.py
+++ script/make_plot_trend.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import subprocess, time, math
+import sys, subprocess, time, math
 
 
 def trendPlot(path, input_filename):
@@ -8,11 +8,13 @@ def trendPlot(path, input_filename):
     potentialValue = data[:, 0]
     gradientSquaredNorm = data[:, 1]
 
-    fig = plt.figure(1, dpi=500, figsize=(7.4, 4.8), facecolor="white")
+    fig = plt.figure(1, dpi=500, figsize=(9, 4.8), facecolor="white")
     ax = fig.add_subplot(111)
     ax.plot(potentialValue, color="orange", label="Potential")
     ax.set_xlabel("Iteration")
     ax.set_ylabel("Value")
+    plt.yticks([-44.3270, -44.3265, -44.3260, -44.3255, -44.3250], ["-44.3270", "-44.3265", "-44.3260", "-44.3255", "-44.3250"])
+    ax.axis([0, len(data[:, 0]), -44.327, -44.325])
     plt.legend()
     plt.savefig(path + "Potential_" + input_filename[:-4] + ".png")
     plt.clf()
@@ -34,7 +36,8 @@ def mk_plot(path, filename, plot_function):
 
 
 if __name__ == "__main__":
-    path = __file__[:-18] + "../output/"
+    # path = __file__[:-18] + "../output3/CG1/"
     # executableCmd = __file__[:-18] + "../src/CG_optimization " + path + "coordinates_rawdata.txt 10 13 " + path
     # subprocess.call(executableCmd, shell=True)
+    path = __file__[:-18] + sys.argv[1] + "/"
     mk_plot(path, "trend.csv", trendPlot)
--- src/CG_optimization.cpp
+++ src/CG_optimization.cpp
@@ -6,7 +6,6 @@
 #include <cmath>
 #include <fstream>
 #include <string>
-// #include <omp.h>
 #include "anneal/utils.h"
 #include "anneal/particle.h"
 #include "anneal/error_handling.h"
@@ -18,7 +17,7 @@ using namespace Eigen;
 class CG {
 
 public:
-    CG(string _output_path, MatrixXd& ensemble);
+    CG(string _output_path);
     ~CG();
 
     inline MatrixXd calc_gradient(MatrixXd& position);
@@ -31,26 +30,26 @@ public:
     // calculate x_ii = x_i + alpha * d_i
     inline MatrixXd line_search(MatrixXd& position, MatrixXd& g_i, MatrixXd& d_i);
 
-    inline void conjugated_gradient_minimization(const double BOX);
+    inline void conjugated_gradient_minimization(const double BOX, MatrixXd& position);
 
     inline void trend_output(double potential_value, double gradient_squaredNorm, ofstream& fout);
 
-    inline void coordinates_output(const double BOX, ofstream& fout);
+    inline void coordinates_output(const double BOX, MatrixXd& position, ofstream& fout);
 
 private:
-    MatrixXd position;                              // store the position of the particles
-    // vector<vector<double> > position;               // store the position of the particles
     double c1;                                      // constant in Wolfe line search
     double c2;                                      // constant in Wolfe line search 0 < c1 < c2 < 1
+    string outputPath;                              // output path
     ofstream trend_out;                             // output file stream of potential gradient norm
+    ofstream coordinates_metastate_out;             // output file stream of metastate coordinates
     ofstream coordinates_out_CG;                    // output file stream of coordinates after CG optimization
 };
 
 
-CG::CG(string _output_path, MatrixXd& ensemble): 
-    position(ensemble),
+CG::CG(string _output_path): 
     c1(1e-4),
     c2(0.8),
+    outputPath(_output_path),
     coordinates_out_CG(_output_path + "/coordinates_after_CG.cif"),
     trend_out(_output_path + "/trend.csv")
     { }
@@ -65,7 +64,6 @@ CG::~CG() {
 MatrixXd CG::calc_gradient(MatrixXd& position) {
     MatrixXd gradient(position.rows(), 3);
     gradient.fill(0);
-    // #pragma omp parallel for
     for (int i = 0; i < position.rows() - 1; ++i) {
         for (int j = i + 1; j < position.rows(); ++j) {
             double dx = position(i, 0) - position(j, 0);
@@ -91,7 +89,6 @@ MatrixXd CG::calc_gradient(MatrixXd& position) {
 
 double CG::calc_potential(MatrixXd &position) {
     double potential_value(0);
-    // #pragma omp parallel for
     for (int i = 0; i < position.rows() - 1; ++i) {
         for (int j = i + 1; j < position.rows(); ++j) {
             double dx = position(i, 0) - position(j, 0);
@@ -143,7 +140,7 @@ MatrixXd CG::line_search(MatrixXd& position, MatrixXd& g_i, MatrixXd& d_i) {
 }
 
 
-void CG::conjugated_gradient_minimization(const double BOX) {
+void CG::conjugated_gradient_minimization(const double BOX, MatrixXd& position) {
     cout << "[MD LOG] " << get_current_time() << "\tConjugated gradient optimization started..." << endl;
     // initialize
     double precision_2(1e-3);
@@ -160,6 +157,7 @@ void CG::conjugated_gradient_minimization(const double BOX) {
     cout << "gradient squared norm: " << gradient_squaredNorm << endl;
     cout << "precision2: " << precision_2 << endl;
     trend_output(potential_value, gradient_squaredNorm, trend_out);
+    // int count(0);   // count number for coordinate output
 
     // minimal condition is the norm of gradient of fx closes to zero
     while (gradient_squaredNorm > precision_2) {
@@ -172,11 +170,14 @@ void CG::conjugated_gradient_minimization(const double BOX) {
         gradient_squaredNorm = g_ii.squaredNorm();
         potential_value = calc_potential(position_ii);
         trend_output(potential_value, gradient_squaredNorm, trend_out);
+        // coordinates_metastate_out.open(outputPath + "/coordinates_metastates_" + to_string(count) +".cif");
+        // coordinates_output(BOX, position_ii, coordinates_metastate_out);
+        // coordinates_metastate_out.close();
+        // count++;
     }
 
     //record final position
-    position = position_ii;
-    coordinates_output(BOX, coordinates_out_CG);
+    coordinates_output(BOX, position_ii, coordinates_out_CG);
     cout << "[MD LOG] " << get_current_time() << "\tConjugated gradient optimization finished." << endl;
 }
 
@@ -186,7 +187,7 @@ void CG::trend_output(double potential_value, double gradient_squaredNorm, ofstr
 }
 
 
-void CG::coordinates_output(const double BOX, ofstream& fout) {
+void CG::coordinates_output(const double BOX, MatrixXd& position, ofstream& fout) {
     fout << "data_structure_1" << endl;
     fout << "_cell_length_a " << BOX << "\n_cell_length_b " << BOX << "\n_cell_length_c " << BOX << endl;
     fout << "_cell_angle_alpha 90\n_cell_angle_beta 90\n_cell_angle_gamma 90" << endl;
@@ -194,7 +195,7 @@ void CG::coordinates_output(const double BOX, ofstream& fout) {
     fout << endl;
     fout << "loop_\n_atom_site_label\n_atom_site_fract_x\n_atom_site_fract_y\n_atom_site_fract_z" << endl;
     for (int i = 0; i < position.rows(); i++) {
-        fout << "O " << position(i, 0) / BOX << " " << position(i, 0) / BOX << " " << position(i, 2) / BOX << endl;
+        fout << "O " << position(i, 0) / BOX << " " << position(i, 1) / BOX << " " << position(i, 2) / BOX << endl;
     }
 }
 
@@ -204,7 +205,6 @@ void CG::coordinates_output(const double BOX, ofstream& fout) {
 int main(int argc, char* argv[]) {
     int BOX = 0;
     int particleNumber = 0;
-    // omp_set_num_threads(32);
     ifstream input;
     string output_path;
     try {
@@ -234,6 +234,6 @@ int main(int argc, char* argv[]) {
         ensemble(i, 2) = stod(coordinateZ);
     }
     
-    CG cg_optimization(output_path, ensemble);
-    cg_optimization.conjugated_gradient_minimization(BOX);
+    CG cg_optimization(output_path);
+    cg_optimization.conjugated_gradient_minimization(BOX, ensemble);
 }
